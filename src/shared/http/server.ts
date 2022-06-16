abba6acfc9e3f9d027831fe32082d92b979a806e
brGuirra <br.guirra@gmail.com>
2022-06-16 10:00:43 -0300

    feat: set up server with express

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,1 +1,11 @@
-console.log('Hello world!')
+import cors from 'cors'
+import express from 'express'
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.listen(3333, () => {
+	console.log('Server started on port 3333 🚀')
+})
