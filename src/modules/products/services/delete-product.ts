c05f874d01ed43f30b7ba481fb1964ecbc6ea569
brGuirra <br.guirra@gmail.com>
2022-06-19 14:45:33 -0300

    feat: add DeleteProduct service

---
+++ src/modules/products/services/delete-product.ts
@@ -0,0 +1,21 @@
+import { AppError } from '@shared/errors/app-error'
+import { getCustomRepository } from 'typeorm'
+import { ProductsRepository } from '../typeorm/repositories/product-repository'
+
+interface IDeleteProduct {
+	id: string
+}
+
+export class DeleteProduct {
+	public async execute({ id }: IDeleteProduct): Promise<void> {
+		const productsRepository = getCustomRepository(ProductsRepository)
+
+		const product = await productsRepository.findOne(id)
+
+		if (!product) {
+			throw new AppError(`Product ${id} not found`)
+		}
+
+		await productsRepository.remove(product)
+	}
+}
