75b0f3e3b542bc3f8083c868b213c0e625295195
brGuirra <br.guirra@gmail.com>
2022-06-19 14:41:54 -0300

    feat: add UpdateProduct service

---
+++ src/modules/products/services/update-product.ts
@@ -0,0 +1,42 @@
+import { AppError } from '@shared/errors/app-error'
+import { getCustomRepository } from 'typeorm'
+import { Product } from '../typeorm/entities/product'
+import { ProductsRepository } from '../typeorm/repositories/product-repository'
+
+interface IUpdateProduct {
+	id: string
+	name: string
+	price: number
+	quantity: number
+}
+
+export class UpdateProduct {
+	public async execute({
+		id,
+		name,
+		price,
+		quantity,
+	}: IUpdateProduct): Promise<Product> {
+		const productsRepository = getCustomRepository(ProductsRepository)
+
+		const product = await productsRepository.findOne(id)
+
+		if (!product) {
+			throw new AppError(`Product ${id} not found`)
+		}
+
+		const productAlreadyExists = await productsRepository.findByName(name)
+
+		if (productAlreadyExists && name !== product.name) {
+			throw new AppError(`Product ${name} already exists`)
+		}
+
+		product.name = name
+		product.price = price
+		product.quantity = quantity
+
+		await productsRepository.save(product)
+
+		return product
+	}
+}
