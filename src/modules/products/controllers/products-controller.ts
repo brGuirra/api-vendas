137dfba167083450fa965d102202968606ef9255
brGuirra <br.guirra@gmail.com>
2022-06-19 14:58:19 -0300

    feat: add a method to get a specific product on ProductsController

--- src/modules/products/controllers/products-controller.ts
+++ src/modules/products/controllers/products-controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express'
 import { ListProducts } from '../services/list-products'
+import { ShowProduct } from '../services/show-product'
 
 export class ProductsController {
 	public async index(request: Request, response: Response): Promise<Response> {
@@ -8,5 +9,15 @@ export class ProductsController {
 		const products = await listProducts.execute()
 
 		return response.json(products)
+	}
+
+	public async show(request: Request, response: Response): Promise<Response> {
+		const { id } = request.params
+
+		const showProduct = new ShowProduct()
+
+		const product = await showProduct.execute({ id })
+
+		return response.json(product)
 	}
 }
