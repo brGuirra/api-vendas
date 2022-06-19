7bfdc9a1f9cfe3cc12db8b7f1788e5f2254e095d
brGuirra <br.guirra@gmail.com>
2022-06-19 15:16:19 -0300

    feat: add a method to create a product on ProductsController

--- src/modules/products/controllers/products-controller.ts
+++ src/modules/products/controllers/products-controller.ts
@@ -1,6 +1,13 @@
 import { Request, Response } from 'express'
+import { CreateProduct } from '../services/create-product'
 import { ListProducts } from '../services/list-products'
 import { ShowProduct } from '../services/show-product'
+
+interface ICreateProductRequest {
+	name: string
+	price: number
+	quantity: number
+}
 
 export class ProductsController {
 	public async index(request: Request, response: Response): Promise<Response> {
@@ -17,6 +24,23 @@ export class ProductsController {
 		const showProduct = new ShowProduct()
 
 		const product = await showProduct.execute({ id })
+
+		return response.json(product)
+	}
+
+	public async create(
+		request: Request<
+			Record<string, unknown>,
+			Record<string, unknown>,
+			ICreateProductRequest
+		>,
+		response: Response
+	): Promise<Response> {
+		const { name, price, quantity } = request.body
+
+		const createProduct = new CreateProduct()
+
+		const product = await createProduct.execute({ name, price, quantity })
 
 		return response.json(product)
 	}
