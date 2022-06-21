cde47b80e8c811110effa80d48035fb6bbd3c1f9
brGuirra <br.guirra@gmail.com>
2022-06-21 17:07:36 -0300

    feat: add sessions controller

---
+++ src/modules/users/controllers/sessions-controller.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from 'express'
+import { CreateSessionsService } from '../services/create-sessions-service'
+
+interface ISessionRequest {
+	email: string
+	password: string
+}
+
+export class SessionsController {
+	async create(
+		request: Request<
+			Record<string, unknown>,
+			Record<string, unknown>,
+			ISessionRequest
+		>,
+		response: Response
+	): Promise<Response> {
+		const { email, password } = request.body
+		const createSessionsService = new CreateSessionsService()
+		const user = await createSessionsService.execute({ email, password })
+
+		return response.json({ user })
+	}
+}
