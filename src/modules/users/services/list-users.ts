ffa66c551cd3e75e146aa4b28c9d8049302471a3
brGuirra <br.guirra@gmail.com>
2022-06-21 15:31:35 -0300

    feat: add list users service

---
+++ src/modules/users/services/list-users.ts
@@ -0,0 +1,18 @@
+import { AppError } from '@shared/errors/app-error'
+import { getCustomRepository } from 'typeorm'
+import { User } from '../typeorm/entities/user'
+import { UsersRepository } from '../typeorm/repositories/users-repository'
+
+export class ListUsers {
+	public async execute(): Promise<User[]> {
+		const usersRepository = getCustomRepository(UsersRepository)
+
+		const users = await usersRepository.find()
+
+		if (users.length === 0) {
+			throw new AppError('No users found')
+		}
+
+		return users
+	}
+}
