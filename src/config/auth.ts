988eb8af9090bc75e92b7ad4fa4ba150e512d1cf
brGuirra <br.guirra@gmail.com>
2022-06-21 18:19:43 -0300

    feat: add auth configuration file for jwt

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export const authConfig = {
+	jwt: {
+		secret: 'd5ed2ddae10572750303e47cc754ed09',
+		expiresIn: '1d',
+	},
+}
--- src/modules/users/services/create-sessions-service.ts
+++ src/modules/users/services/create-sessions-service.ts
@@ -2,6 +2,7 @@ import { AppError } from '@shared/errors/app-error'
 import { compare } from 'bcryptjs'
 import { getCustomRepository } from 'typeorm'
 import { sign } from 'jsonwebtoken'
+import { authConfig } from '@config/auth'
 import { User } from '../typeorm/entities/user'
 import { UsersRepository } from '../typeorm/repositories/users-repository'
 
@@ -33,9 +34,9 @@ export class CreateSessionsService {
 			throw new AppError('Invalid email or password', 401)
 		}
 
-		const token = sign({}, 'd5ed2ddae10572750303e47cc754ed09', {
+		const token = sign({}, authConfig.jwt.secret, {
 			subject: user.id,
-			expiresIn: '1d',
+			expiresIn: authConfig.jwt.expiresIn,
 		})
 
 		return { user, token }
