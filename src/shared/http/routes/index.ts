0bc30ad8ef51575959fae6779519b4422f18aa5a
brGuirra <br.guirra@gmail.com>
2022-06-21 17:13:49 -0300

    feat: add sessions route

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,4 +1,5 @@
 import { productsRouter } from '@modules/products/routes/products-router'
+import { sessionsRouter } from '@modules/users/routes/sessions-router'
 import { usersRouter } from '@modules/users/routes/users-router'
 import { Router } from 'express'
 
@@ -6,3 +7,4 @@ export const routes = Router()
 
 routes.use('/products', productsRouter)
 routes.use('/users', usersRouter)
+routes.use('/sessions', sessionsRouter)
