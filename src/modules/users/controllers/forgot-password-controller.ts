dc08535c79b20941e7eb6551c0662708188a1991
brGuirra <br.guirra@gmail.com>
2022-06-22 12:39:21 -0300

    feat: add ForgotPasswordController

---
+++ src/modules/users/controllers/forgot-password-controller.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from 'express'
+import { SendForgotPasswordEmailService } from '../services/send-forgot-password-email-service'
+
+interface IForgotPasswrordRequest {
+	email: string
+}
+
+export class ForgotPasswordController {
+	async create(
+		request: Request<
+			Record<string, unknown>,
+			Record<string, unknown>,
+			IForgotPasswrordRequest
+		>,
+		response: Response
+	): Promise<Response> {
+		const { email } = request.body
+		const sendForgotPasswordEmailService = new SendForgotPasswordEmailService()
+		await sendForgotPasswordEmailService.execute({ email })
+
+		return response.status(204).json()
+	}
+}
