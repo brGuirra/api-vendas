15fdf6485f65c578f146611f92e8218cda59f47a
brGuirra <br.guirra@gmail.com>
2022-06-22 17:21:59 -0300

    feat: add a service to retreive information about an user

---
+++ src/modules/users/services/show-profile-service.ts
@@ -0,0 +1,22 @@
+import { AppError } from '@shared/errors/app-error'
+import { getCustomRepository } from 'typeorm'
+import { User } from '../typeorm/entities/user'
+import { UsersRepository } from '../typeorm/repositories/users-repository'
+
+interface IShowProfileRequest {
+	user_id: string
+}
+
+export class ShowProfileService {
+	public async execute({ user_id }: IShowProfileRequest): Promise<User> {
+		const usersRepository = getCustomRepository(UsersRepository)
+
+		const user = await usersRepository.findById(user_id)
+
+		if (!user) {
+			throw new AppError('User not found')
+		}
+
+		return user
+	}
+}
