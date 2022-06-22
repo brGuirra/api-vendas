92c0542b2590c811ffe2162537fd9c21cea21d48
brGuirra <br.guirra@gmail.com>
2022-06-22 12:19:37 -0300

    feat: add a service to generate tokens for users

---
+++ src/modules/users/services/send-forgot-password-email-service.ts
@@ -0,0 +1,22 @@
+import { AppError } from '@shared/errors/app-error'
+import { getCustomRepository } from 'typeorm'
+import { UserTokensRepository } from '../typeorm/repositories/user-tokens-repository'
+import { UsersRepository } from '../typeorm/repositories/users-repository'
+
+interface IForgotPasswordEmail {
+	email: string
+}
+
+export class SendForgotPasswordEmailService {
+	public async execute({ email }: IForgotPasswordEmail): Promise<void> {
+		const usersRepository = getCustomRepository(UsersRepository)
+		const userTokensRepository = getCustomRepository(UserTokensRepository)
+		const user = await usersRepository.findByEmail(email)
+
+		if (!user) {
+			throw new AppError('User does not exist')
+		}
+
+		const userToken = await userTokensRepository.generate(user.id)
+	}
+}
