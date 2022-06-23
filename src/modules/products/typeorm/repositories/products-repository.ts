22302fcf0cb31aa67317ea7217b6e78cdcb2519b
brGuirra <br.guirra@gmail.com>
2022-06-23 18:48:23 -0300

    feat: add findAllByIds method in ProductsRepository

--- src/modules/products/typeorm/repositories/products-repository.ts
+++ src/modules/products/typeorm/repositories/products-repository.ts
@@ -1,12 +1,26 @@
-import { EntityRepository, Repository } from 'typeorm'
+import { EntityRepository, In, Repository } from 'typeorm'
 
 import { Product } from '../entities/product'
 
+interface IFindProducts {
+	id: string
+}
+
 @EntityRepository(Product)
 export class ProductsRepository extends Repository<Product> {
-	public async findByName(name: string): Promise<Product | undefined> {
+	public async findByName(name: string): Promise<Product> {
 		return this.findOne({
 			where: { name },
+		})
+	}
+
+	public async findAllByIds(products: IFindProducts[]): Promise<Product[]> {
+		const productsIds = products.map(product => product.id)
+
+		return this.find({
+			where: {
+				id: In(productsIds),
+			},
 		})
 	}
 }
