43a986f8512b9ac5efc9c27842a1f75e90b497e2
brGuirra <br.guirra@gmail.com>
2022-06-23 14:15:05 -0300

    feat: add customers route

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,3 +1,4 @@
+import { customersRouter } from '@modules/customers/routes/customer-router'
 import { productsRouter } from '@modules/products/routes/products-router'
 import { passwordRouter } from '@modules/users/routes/password-router'
 import { profileRouter } from '@modules/users/routes/profile-router'
@@ -12,3 +13,4 @@ routes.use('/users', usersRouter)
 routes.use('/sessions', sessionsRouter)
 routes.use('/password', passwordRouter)
 routes.use('/profile', profileRouter)
+routes.use('/customers', customersRouter)
