49562d1a59627d3cacd7e70bd6656eec1ab38689
brGuirra <br.guirra@gmail.com>
2022-06-24 15:37:49 -0300

    feat: add a method to recover data from redis

--- src/shared/cache/redis-cache.ts
+++ src/shared/cache/redis-cache.ts
@@ -11,4 +11,14 @@ export class RedisCache {
 	public async save(key: string, value: any): Promise<void> {
 		await this.client.set(key, JSON.stringify(value))
 	}
+
+	public async recover<T>(key: string): Promise<T | undefined> {
+		const data = await this.client.get(key)
+
+		if (!data) {
+			return undefined
+		}
+
+		return JSON.parse(data) as T
+	}
 }
