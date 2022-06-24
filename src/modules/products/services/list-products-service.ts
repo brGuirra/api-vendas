5340ff3ac01e0551985953ce561a3816a0dee8cb
brGuirra <br.guirra@gmail.com>
2022-06-24 15:45:26 -0300

    feat: add cache to ListProductsService

--- src/modules/products/services/list-products-service.ts
+++ src/modules/products/services/list-products-service.ts
@@ -1,3 +1,4 @@
+import { RedisCache } from '@shared/cache/redis-cache'
 import { AppError } from '@shared/errors/app-error'
 import { getCustomRepository } from 'typeorm'
 import { Product } from '../typeorm/entities/product'
@@ -6,11 +7,20 @@ import { ProductsRepository } from '../typeorm/repositories/products-repository'
 export class ListProductsService {
 	public async execute(): Promise<Product[]> {
 		const productsRepository = getCustomRepository(ProductsRepository)
+		const redisCache = new RedisCache()
 
-		const products = await productsRepository.find()
+		let products = await redisCache.recover<Product[]>(
+			'api-vendas-PRODUCT_LIST'
+		)
 
-		if (products.length === 0) {
-			throw new AppError('No products found')
+		if (!products) {
+			products = await productsRepository.find()
+
+			if (products.length === 0) {
+				throw new AppError('No products found')
+			}
+
+			await redisCache.save('api-vendas-PRODUCT_LIST', products)
 		}
 
 		return products
